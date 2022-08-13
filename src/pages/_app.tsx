b541cb502c320fa0b48843fef49e1cbc5a6e024a
Darlan H <darlanhermessjb@hotmail.com>
2022-08-13 14:35:31 -0300

    fix: consider public url on client config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -37,6 +37,8 @@ export default withTRPC<AppRouter>({
      */
     const url = process.env.VERCEL_URL
       ? `https://${process.env.VERCEL_URL}/api/trpc`
+      : process.env.NEXT_PUBLIC_VERCEL_URL
+      ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/trpc`
       : 'http://localhost:3000/api/trpc';
 
     return {
